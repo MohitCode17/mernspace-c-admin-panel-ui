e2144c5bc09cc9f9db8697dd3d717295b2fdbd13
MohitCode17 <mohit.codes17@gmail.com>
2025-01-12 23:32:06 +0530

    add login endpoint

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,8 +1,35 @@
-import { Card, Layout, Space, Button, Checkbox, Form, Input, Flex } from "antd";
+import {
+  Card,
+  Layout,
+  Space,
+  Button,
+  Checkbox,
+  Form,
+  Input,
+  Flex,
+  Alert,
+} from "antd";
 import { LockFilled, LockOutlined, UserOutlined } from "@ant-design/icons";
 import Logo from "../../components/icons/Logo";
+import { useMutation } from "@tanstack/react-query";
+import { Credentials } from "../../types";
+import { login } from "../../http/api";
 
 const LoginPage = () => {
+  const loginUser = async (credentials: Credentials) => {
+    // server login for login user
+    const { data } = await login(credentials);
+    return data;
+  };
+
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationKey: ["login"],
+    mutationFn: loginUser,
+    onSuccess: async () => {
+      console.log("Login successful");
+    },
+  });
+
   return (
     <>
       <Layout
@@ -28,7 +55,19 @@ const LoginPage = () => {
               </Space>
             }
           >
-            <Form initialValues={{ remember: true }}>
+            <Form
+              initialValues={{ remember: true }}
+              onFinish={(values) => {
+                mutate({ email: values.username, password: values.password });
+              }}
+            >
+              {isError && (
+                <Alert
+                  style={{ marginBottom: 24 }}
+                  type="error"
+                  message={error.message}
+                />
+              )}
               <Form.Item
                 name={"username"}
                 rules={[
@@ -65,6 +104,7 @@ const LoginPage = () => {
                   type="primary"
                   htmlType="submit"
                   style={{ width: "100%" }}
+                  loading={isPending}
                 >
                   Log in
                 </Button>
