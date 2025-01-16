f040f4a363cf71f01cd90fa59f2caa503cc13a80
MohitCode17 <mohit.codes17@gmail.com>
2025-01-16 23:36:32 +0530

    create user form ui

--- src/pages/users/form/UserForm.tsx
+++ src/pages/users/form/UserForm.tsx
@@ -1,23 +1,106 @@
-import { Card, Col, Form, Input, Row } from "antd";
+import { Card, Col, Form, Input, Row, Select, Space } from "antd";
 
 const UserForm = () => {
   return (
     <Row>
       <Col span={24}>
-        <Card title="Basic info" bordered={false}>
-          <Row gutter={20}>
-            <Col span={12}>
-              <Form.Item label="First name" name="firstName">
-                <Input />
-              </Form.Item>
-            </Col>
-            <Col span={12}>
-              <Form.Item label="Last name" name="lastName">
-                <Input />
-              </Form.Item>
-            </Col>
-          </Row>
-        </Card>
+        <Space direction="vertical" size={"large"} style={{ width: "100%" }}>
+          <Card title="Basic info" bordered={false}>
+            <Row gutter={20}>
+              <Col span={12}>
+                <Form.Item label="First name" name="firstName">
+                  <Input />
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item label="Last name" name="lastName">
+                  <Input />
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item label="Email" name="email">
+                  <Input />
+                </Form.Item>
+              </Col>
+            </Row>
+          </Card>
+
+          <Card title="Security info" bordered={false}>
+            <Row gutter={20}>
+              <Col span={12}>
+                <Form.Item
+                  label="Passoword"
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Password required",
+                    },
+                  ]}
+                >
+                  <Input type="password" />
+                </Form.Item>
+              </Col>
+            </Row>
+          </Card>
+
+          <Card title="Role" bordered={false}>
+            <Row gutter={20}>
+              <Col span={12}>
+                <Form.Item
+                  label="Role"
+                  name="role"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Role is required",
+                    },
+                  ]}
+                >
+                  <Select
+                    style={{ width: "100%" }}
+                    placeholder="Select role"
+                    allowClear
+                    options={[
+                      { value: "admin", label: "Admin" },
+                      { value: "manager", label: "Manager" },
+                      { value: "customer", label: "Customer" },
+                    ]}
+                  />
+                </Form.Item>
+              </Col>
+
+              <Col span={12}>
+                <Form.Item
+                  label="Restaurant"
+                  name="tenantId"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Role is required",
+                    },
+                  ]}
+                >
+                  <Select
+                    style={{ width: "100%" }}
+                    placeholder="Select restaurant"
+                    allowClear
+                  >
+                    <Select.Option value="restaurant1">
+                      Restaurant 1
+                    </Select.Option>
+                    <Select.Option value="restaurant2">
+                      Restaurant 2
+                    </Select.Option>
+                    <Select.Option value="restaurant3">
+                      Restaurant 3
+                    </Select.Option>
+                  </Select>
+                </Form.Item>
+              </Col>
+            </Row>
+          </Card>
+        </Space>
       </Col>
     </Row>
   );
