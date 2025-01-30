8abeff5c42b8be580650d3657450f1eb6ebb797f
MohitCode17 <mohit.codes17@gmail.com>
2025-01-30 22:22:24 +0530

    refactor product form to use separate ProductImage component for image upload

--- src/pages/products/form/ProductForm.tsx
+++ src/pages/products/form/ProductForm.tsx
@@ -3,26 +3,20 @@ import {
   Col,
   Form,
   Input,
-  message,
   Row,
   Select,
   Space,
   Switch,
   Typography,
-  Upload,
-  UploadProps,
 } from "antd";
-import { PlusOutlined } from "@ant-design/icons";
 import { useQuery } from "@tanstack/react-query";
 import { getCategories, getTenants } from "../../../http/api";
 import { Category, Tenant } from "../../../types";
 import Pricing from "./Pricing";
 import Attributes from "./Attributes";
-import { useState } from "react";
+import ProductImage from "./ProductImage";
 
 const ProductForm = () => {
-  const [messageApi, contextHolder] = message.useMessage();
-  const [imageUrl, setImageUrl] = useState<string | null>(null);
   const selectedCategory = Form.useWatch("categoryId");
 
   // Get all restaurants list
@@ -42,26 +36,6 @@ const ProductForm = () => {
       return res.data;
     },
   });
-
-  const uploaderConfig: UploadProps = {
-    name: "file",
-    multiple: false,
-    showUploadList: false,
-    // Prevent automatic upload
-    beforeUpload: (file) => {
-      // Validation logic
-      const isJpgOrPng =
-        file.type === "image/jpeg" || file.type === "image/png";
-
-      if (!isJpgOrPng) {
-        console.log("You can only upload JPG/PNG file!");
-        messageApi.error("You can only upload JPG/PNG file!");
-      }
-
-      setImageUrl(URL.createObjectURL(file));
-      return false;
-    },
-  };
 
   return (
     <Row>
@@ -134,31 +108,7 @@ const ProductForm = () => {
           <Card title="Product Image" bordered={false}>
             <Row gutter={20}>
               <Col span={12}>
-                <Form.Item
-                  name="image"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Please upload a product image",
-                    },
-                  ]}
-                >
-                  {contextHolder}
-                  <Upload listType="picture-card" {...uploaderConfig}>
-                    {imageUrl ? (
-                      <img
-                        src={imageUrl}
-                        alt="product_image"
-                        style={{ width: "100%" }}
-                      />
-                    ) : (
-                      <Space direction="vertical">
-                        <PlusOutlined />
-                        <Typography.Text>Upload</Typography.Text>
-                      </Space>
-                    )}
-                  </Upload>
-                </Form.Item>
+                <ProductImage />
               </Col>
             </Row>
           </Card>
