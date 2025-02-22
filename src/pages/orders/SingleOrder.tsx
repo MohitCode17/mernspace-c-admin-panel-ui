1d107a7aae525c77e1395c03964f42460afdd010
MohitCode17 <mohit.codes17@gmail.com>
2025-02-22 20:13:55 +0530

    add customer details to single order page

--- src/pages/orders/SingleOrder.tsx
+++ src/pages/orders/SingleOrder.tsx
@@ -47,7 +47,7 @@ const SingleOrder = () => {
           items={[
             { title: <Link to={"/"}>Dashboard</Link> },
             { title: <Link to={"/orders"}>Orders</Link> },
-            { title: `Orders #12345678978978` },
+            { title: `Orders #${order._id}` },
           ]}
         />
       </Flex>
@@ -95,7 +95,58 @@ const SingleOrder = () => {
           </Card>
         </Col>
         <Col span={10}>
-          <Card title="Customer Details">some content goes here...</Card>
+          <Card title="Customer Details">
+            <Space direction="vertical">
+              <Flex style={{ flexDirection: "column" }}>
+                <Typography.Text type="secondary">Name</Typography.Text>
+                <Typography.Text>
+                  {order.customerId.firstName + " " + order.customerId.lastName}
+                </Typography.Text>
+              </Flex>
+
+              <Flex style={{ flexDirection: "column" }}>
+                <Typography.Text type="secondary">Address</Typography.Text>
+                <Typography.Text>{order.address}</Typography.Text>
+              </Flex>
+
+              <Flex style={{ flexDirection: "column" }}>
+                <Typography.Text type="secondary">
+                  Payment Method
+                </Typography.Text>
+                <Typography.Text>
+                  {order.paymentMode.toUpperCase()}
+                </Typography.Text>
+              </Flex>
+
+              <Flex style={{ flexDirection: "column" }}>
+                <Typography.Text type="secondary">
+                  Payment Status
+                </Typography.Text>
+                <Typography.Text>
+                  {capitalize(order.paymentStatus)}
+                </Typography.Text>
+              </Flex>
+
+              <Flex style={{ flexDirection: "column" }}>
+                <Typography.Text type="secondary">Order Amount</Typography.Text>
+                <Typography.Text>₹{order.total}</Typography.Text>
+              </Flex>
+
+              <Flex style={{ flexDirection: "column" }}>
+                <Typography.Text type="secondary">Order Time</Typography.Text>
+                <Typography.Text>
+                  {new Date(order.createdAt).toLocaleString()}
+                </Typography.Text>
+              </Flex>
+
+              {order.comment && (
+                <Flex style={{ flexDirection: "column" }}>
+                  <Typography.Text type="secondary">Comment</Typography.Text>
+                  <Typography.Text>{order.comment}</Typography.Text>
+                </Flex>
+              )}
+            </Space>
+          </Card>
         </Col>
       </Row>
     </Space>
